76404ef808ec57bc55cd44477050f69c34555f5c
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2022-11-20 20:11:10 -0300

    ajustando a detecção

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Flutter project with live camera
 
 ## Description
 
-This project use live camera to capture images.
+This project use live camera to capture images, and mltoolkit to predict image label.
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 
 late List<CameraDescription> _cameras;
 
@@ -36,21 +38,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late CameraController cameraController;
+  late CameraController _cameraController;
+  late ImageLabeler _imageLabeler;
+  List<ImageLabel>? _labels;
+
+  bool isProcessingImage = false;
 
   @override
   void initState() {
     super.initState();
 
-    cameraController = CameraController(
+    _cameraController = CameraController(
       _cameras[0],
       ResolutionPreset.high,
     );
 
-    cameraController.initialize().then((_) {
+    ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.5);
+
+    _imageLabeler = ImageLabeler(options: options);
+
+    _labels = <ImageLabel>[];
+
+    _cameraController.initialize().then((_) {
       if (!mounted) {
         return;
       }
+      // Start to send camera image to function that will compute
+      // the image label and set the [isProcessingImage] variable to true
+      // remenber, arrow functions can execute more than one function
+      _cameraController.startImageStream((image) => {
+            if (isProcessingImage == false)
+              {
+                doImageLabeling(image),
+                isProcessingImage = true,
+              }
+          });
 
       setState(() {});
     }).catchError((Object e) {
@@ -71,14 +93,105 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     super.dispose();
 
-    cameraController.dispose();
+    _cameraController.stopImageStream();
+
+    _cameraController.dispose();
+
+    _imageLabeler.close();
+  }
+
+  InputImage getInputImage(CameraImage img) {
+    final WriteBuffer allBytes = WriteBuffer();
+
+    for (final Plane plane in img.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize = Size(img.width.toDouble(), img!.height.toDouble());
+
+    final camera = _cameras[0];
+
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+
+    final inputImageFormat = InputImageFormatValue.fromRawValue(img.format.raw);
+
+    final planeData = img.planes
+        .map(
+          (plane) => InputImagePlaneMetadata(
+            bytesPerRow: plane.bytesPerRow,
+            height: plane.height,
+            width: plane.width,
+          ),
+        )
+        .toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: inputImageFormat!,
+      planeData: planeData,
+    );
+
+    return InputImage.fromBytes(
+      bytes: bytes,
+      inputImageData: inputImageData,
+    );
+  }
+
+  doImageLabeling(CameraImage img) async {
+    final InputImage inputImage = getInputImage(img);
+
+    if (_labels?.isNotEmpty ?? false) _labels?.clear();
+
+    final newLabels = await _imageLabeler.processImage(inputImage);
+
+    setState(() {
+      _labels = newLabels;
+    });
+
+    // set [isProcessingImage] to false, 
+    // releasing to next frame be processed
+    isProcessingImage = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CameraPreview(
-        cameraController,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: 200,
+              child: CameraPreview(
+                _cameraController,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  if (_labels?.isNotEmpty ?? false)
+                    ..._labels!
+                        .map(
+                          (e) => Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              '${e.label}, ${e.index}, ${e.confidence.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList()
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
